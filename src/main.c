82648a9e8f7b8368267cc1cc9b8f8f7ea4c6b12f
ztnel <christian911@sympatico.ca>
2023-01-10 00:18:55 -0500

    autofmt

--- src/main.c
+++ src/main.c
@@ -1,25 +1,27 @@
 /**
  * @file main.c
  * @author Christian Sargusingh (christian911@sympatico.ca)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2023-01
- * 
+ *
  * @copyright Copyright © 2023 dronectl
- * 
+ *
  */
 
-#include <avr/io.h>
 #include <avr/cpufunc.h>
+#include <avr/io.h>
 
 static void __delay(uint64_t cycles) {
-  for (uint64_t i = 0; i < cycles; i++) { _NOP(); };
+  for (uint64_t i = 0; i < cycles; i++) {
+    _NOP();
+  };
 }
 
 static void spinlock(void) {
   // Setup PB5 as Output high (source)
-  PORTB = (1<<PB5);
-  DDRB = (1<<DDB5);
+  PORTB = (1 << PB5);
+  DDRB = (1 << DDB5);
   // sync nop
   _NOP();
   while (1) {
