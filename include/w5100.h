c8abfe662b775540a714971a073787e0c10f8db6
ztnel <christian911@sympatico.ca>
2023-01-14 17:18:35 -0500

    linking fixes, status codes and function renaming

--- include/w5100.h
+++ include/w5100.h
@@ -1,7 +1,7 @@
 /**
  * @file w5100.h
  * @brief Functions declarations and definitions for interfacing with the w5100
- * ethernet controller.
+ * ethernet phy controller.
  * @version 0.1
  * @date 2023-01
  *
@@ -12,6 +12,7 @@
 #ifndef __W5100_H__
 #define __W5100_H__
 
+#include "spi.h"
 #include <avr/io.h>
 #include <stdint.h>
 
@@ -20,6 +21,18 @@
 #else
 #define W5100_MAX_SOCK_NUM 4
 #endif
+
+/**
+ * @brief W5100 API Status Codes
+ *
+ */
+typedef uint8_t w5100_status_t;
+#define W5100_OK (w5100_status_t)0  // success code
+#define W5100_ERR (w5100_status_t)1 // generic failure
+
+// static for multiple translation units
+static const spi_config_t w5100_spi_config = {
+    .clock = 14000000, .endian = BIG_ENDIAN, .mode = SPI_MODE0};
 
 /**
  * @brief Mode Register Bits
@@ -31,66 +44,79 @@
 #define MR_AI (uint8_t)(1 << 1)    // Address Auto-Increment in Indirect Bus I/F
 #define MR_IND (uint8_t)(1 << 0)   // Indirect Bus I/F mode
 
-#define __GP_REGISTER8(name, address)                                          \
-  static inline void write##name(uint8_t _data) {                              \
-    w5100_write_byte(address, _data);                                          \
+void _read_byte(uint16_t addr, uint8_t *buffer);
+void _write_byte(uint16_t addr, const uint8_t buffer);
+uint16_t _read_bytes(uint16_t addr, uint8_t *buffer, uint16_t len);
+uint16_t _write_bytes(uint16_t addr, const uint8_t *buffer, uint16_t len);
+
+/**
+ * @brief Common 8 bit register read and write functions
+ *
+ */
+#define __CREGISTER8(name, address)                                            \
+  static void w5100_write_##name(uint8_t _data) {                              \
+    _write_byte(address, _data);                                               \
   }                                                                            \
-  static inline uint8_t read##name(void) {                                     \
+  static uint8_t w5100_read_##name(void) {                                     \
     uint8_t data;                                                              \
-    w5100_read_byte(address, &data);                                           \
+    _read_byte(address, &data);                                                \
     return data;                                                               \
   }
 
-#define __GP_REGISTER16(name, address)                                         \
-  static uint16_t write##name(uint16_t _data) {                                \
+/**
+ * @brief Common 16 bit register read and write functions
+ *
+ */
+#define __CREGISTER16(name, address)                                           \
+  static uint16_t w5100_write_##name(const uint16_t _data) {                   \
     uint8_t buf[2];                                                            \
     buf[0] = _data >> 8;                                                       \
     buf[1] = _data & 0xFF;                                                     \
-    return w5100_write_bytes(address, buf, 2);                                 \
+    return _write_bytes(address, buf, 2);                                      \
   }                                                                            \
-  static uint16_t read##name(void) {                                           \
+  static uint16_t w5100_read_##name(void) {                                    \
     uint8_t buf[2];                                                            \
-    w5100_read_bytes(address, buf, 2);                                         \
+    read_bytes(address, buf, 2);                                               \
     return (buf[0] << 8) | buf[1];                                             \
   }
 
-#define __GP_REGISTER_N(name, address, size)                                   \
-  static uint16_t write##name(const uint8_t *_buffer) {                        \
-    return w5100_write_bytes(address, _buffer, size);                          \
+/**
+ * @brief Common N-bit register read and write functions
+ *
+ */
+#define __CREGISTER_N(name, address, size)                                     \
+  static uint16_t w5100_write_##name(const uint8_t *_buffer) {                 \
+    return _write_bytes(address, _buffer, size);                               \
   }                                                                            \
-  static uint16_t read##name(uint8_t *_buffer) {                               \
-    return w5100_read_bytes(address, _buffer, size);                           \
+  static uint16_t w5100_read_##name(uint8_t *_buffer) {                        \
+    return _read_bytes(address, _buffer, size);                                \
   }
 
-void w5100_read_byte(uint16_t addr, uint8_t *buffer);
-void w5100_write_byte(uint16_t addr, const uint8_t buffer);
-uint16_t w5100_read_bytes(uint16_t addr, uint8_t *buffer, uint16_t len);
-uint16_t w5100_write_bytes(uint16_t addr, const uint8_t *buffer, uint16_t len);
-void w5100_init(void);
-uint8_t w5100_soft_reset(void);
+w5100_status_t w5100_verify_hw(void);
+w5100_status_t w5100_reset(void);
 
 /**
  * @brief Common register definitions
  *
  * 4.1 Common Registers
  */
-__GP_REGISTER8(_mr, 0x0000)       // mode register
-__GP_REGISTER_N(_gar, 0x0001, 4)  // gateway address register
-__GP_REGISTER_N(_subr, 0x0005, 4) // subnet mask address register
-__GP_REGISTER_N(_shar, 0x0009, 6) // source hardware address register
-__GP_REGISTER_N(_sipr, 0x000F, 4) // source ip address register
-__GP_REGISTER8(_ir, 0x0015)       // interrupt register
-__GP_REGISTER8(_imr, 0x0016)      // interrupt mask register
-__GP_REGISTER16(_rtr, 0x0017)     // retry time-value register
-__GP_REGISTER8(_rcr, 0x0019)      // retry count register
-__GP_REGISTER8(_rmsr, 0x001A)     // rx memory size register
-__GP_REGISTER8(_tmsr, 0x001B)     // tx memory size register
-__GP_REGISTER8(_patr, 0x001C)     // authentication type in PPPoE mode
-__GP_REGISTER8(_ptimer,
-               0x0028) // PPP link control protocol request timer register
-__GP_REGISTER8(_pmagic,
-               0x0029) // PPP link control protocol magic number register
-__GP_REGISTER_N(_uipr, 0x002A, 4) // unreachable ip address register (UDP Mode)
-__GP_REGISTER16(_uport, 0x002E)   // unreachable port register (UDP Mode)
+__CREGISTER8(mr, 0x0000)      // mode register
+__CREGISTER_N(gar, 0x0001, 4) // gateway address register
+// __CREGISTER_N(subr, 0x0005, 4) // subnet mask address register
+// __CREGISTER_N(shar, 0x0009, 6) // source hardware address register
+// __CREGISTER_N(sipr, 0x000F, 4) // source ip address register
+// __CREGISTER8(ir, 0x0015)       // interrupt register
+// __CREGISTER8(imr, 0x0016)      // interrupt mask register
+// __CREGISTER_N(rtr, 0x0017, 2)  // retry time-value register
+// __CREGISTER8(rcr, 0x0019)      // retry count register
+// __CREGISTER8(rmsr, 0x001A)     // rx memory size register
+// __CREGISTER8(tmsr, 0x001B)     // tx memory size register
+// __CREGISTER8(patr, 0x001C)     // authentication type in PPPoE mode
+// __CREGISTER8(ptimer,
+//              0x0028) // PPP link control protocol request timer register
+// __CREGISTER8(pmagic,
+//              0x0029) // PPP link control protocol magic number register
+// __CREGISTER_N(uipr, 0x002A, 4)  // unreachable ip address register (UDP Mode)
+// __CREGISTER_N(uport, 0x002E, 2) // unreachable port register (UDP Mode)
 
 #endif // __W5100_H__
