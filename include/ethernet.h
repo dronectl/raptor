897240d98d9f3a983a243a43a61334b32c89f706
ztnel <christian911@sympatico.ca>
2023-01-14 18:30:08 -0500

    implement remaining core property methods

--- include/ethernet.h
+++ include/ethernet.h
@@ -1,11 +1,11 @@
 /**
  * @file ethernet.h
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2023-01
- * 
+ *
  * @copyright Copyright © 2023 dronectl
- * 
+ *
  */
 #ifndef __ETHERNET_H__
 #define __ETHERNET_H__
@@ -13,23 +13,31 @@
 #include <stdint.h>
 
 /**
+ * @brief Ethernet Proxy API Status Codes
+ *
+ */
+typedef uint8_t enet_status_t;
+#define ENET_OK (enet_status_t)0  // success code
+#define ENET_ERR (enet_status_t)1 // generic failure
+
+/**
  * @brief IPv4 address representation types
- * 
+ *
  */
 typedef union ipv4_address_t {
   uint8_t bytes[4]; // for byte data transactions
-  uint32_t dword; // for comparator ops
+  uint32_t dword;   // for comparator ops
 } ipv4_address_t;
 
-void ethernet_init(void);
-void ethernet_set_gateway(const ipv4_address_t addr);
-void ethernet_get_gateway(ipv4_address_t *addr);
-void ethernet_set_subnet(const uint8_t *addr);
-void ethernet_get_subnet(uint8_t *addr);
+enet_status_t ethernet_phy_init(void);
+void ethernet_set_gateway(const ipv4_address_t gateway);
+void ethernet_get_gateway(ipv4_address_t *gateway, uint16_t *len);
+void ethernet_set_subnet_mask(const ipv4_address_t subnet_mask);
+void ethernet_get_subnet_mask(ipv4_address_t *subnet_mask, uint16_t *len);
 void ethernet_set_mac_addr(const uint8_t *addr);
-void ethernet_get_mac_addr(uint8_t *addr);
-void ethernet_set_ip_addr(const uint8_t *addr);
-void ethernet_get_ip_addr(uint8_t *addr);
+void ethernet_get_mac_addr(uint8_t *addr, uint16_t *len);
+void ethernet_set_ip_addr(const ipv4_address_t ip_addr);
+void ethernet_get_ip_addr(ipv4_address_t *ip_addr, uint16_t *len);
 void ethernet_set_retransmit_time(uint16_t timeout);
 void ethernet_set_retransmit_count(uint8_t retry);
 void ethernet_reset(void);
--- src/ethernet.c
+++ src/ethernet.c
@@ -13,20 +13,116 @@
 #include "spi.h"
 #include "w5100.h"
 
-void ethernet_init(void) {
+/**
+ * @brief Initialize ethernet phy chip.
+ *
+ */
+enet_status_t ethernet_phy_init(void) {
+  enet_status_t status = ENET_OK;
   spi_begin(w5100_spi_config);
-  w5100_verify_hw();
+  if (w5100_verify_hw() != W5100_OK)
+    status = ENET_ERR;
   spi_end();
+  return status;
 }
 
+/**
+ * @brief Write ethernet gateway to ethernet phy
+ *
+ * @param gateway gateway raw bytes
+ */
 void ethernet_set_gateway(const ipv4_address_t gateway) {
   spi_begin(w5100_spi_config);
   w5100_write_gar(gateway.bytes);
   spi_end();
 }
 
-void ethernet_get_gateway(ipv4_address_t *gateway) {
+/**
+ * @brief Query ethernet gateway from ethernet phy
+ *
+ * @param gateway ipv4_address_t pointer buffer
+ * @param len number of bytes read from transaction
+ */
+void ethernet_get_gateway(ipv4_address_t *gateway, uint16_t *len) {
+  assert(len != NULL);
+  assert(gateway != NULL);
   spi_begin(w5100_spi_config);
-  w5100_read_gar(gateway->bytes);
+  *len = w5100_read_gar(gateway->bytes);
+  spi_end();
+}
+
+/**
+ * @brief Write ethernet subnet mask to ethernet phy
+ *
+ * @param subnet_mask subnet mask raw bytes
+ */
+void ethernet_set_subnet_mask(const ipv4_address_t subnet_mask) {
+  spi_begin(w5100_spi_config);
+  w5100_write_subr(subnet_mask.bytes);
+  spi_end();
+}
+
+/**
+ * @brief Query ethernet subnet mask from ethernet phy
+ *
+ * @param subnet_mask ipv4_address_t pointer buffer
+ * @param len number of bytes read from transaction
+ */
+void ethernet_get_subnet_mask(ipv4_address_t *subnet_mask, uint16_t *len) {
+  assert(len != NULL);
+  assert(subnet_mask != NULL);
+  spi_begin(w5100_spi_config);
+  *len = w5100_read_subr(subnet_mask->bytes);
+  spi_end();
+}
+
+/**
+ * @brief Write ethernet ip address to ethernet phy
+ *
+ * @param ip_addr ip address raw bytes
+ */
+void ethernet_set_ip_addr(const ipv4_address_t ip_addr) {
+  spi_begin(w5100_spi_config);
+  w5100_write_sipr(ip_addr.bytes);
+  spi_end();
+}
+
+/**
+ * @brief Query ethernet ip address from ethernet phy
+ *
+ * @param ip_addr ipv4_address_t pointer buffer
+ * @param len number of bytes read from transaction
+ */
+void ethernet_get_ip_addr(ipv4_address_t *ip_addr, uint16_t *len) {
+  assert(len != NULL);
+  assert(ip_addr != NULL);
+  spi_begin(w5100_spi_config);
+  *len = w5100_read_sipr(ip_addr->bytes);
+  spi_end();
+}
+
+/**
+ * @brief Write ethernet mac address to ethernet phy
+ *
+ * @param mac_addr mac address raw bytes
+ */
+void ethernet_set_mac_addr(const uint8_t *mac_addr) {
+  assert(mac_addr != NULL);
+  spi_begin(w5100_spi_config);
+  w5100_write_sipr(mac_addr);
+  spi_end();
+}
+
+/**
+ * @brief Query ethernet mac address from ethernet phy
+ *
+ * @param mac_addr uint8_t pointer buffer
+ * @param len number of bytes read from transaction
+ */
+void ethernet_get_mac_addr(uint8_t *mac_addr, uint16_t *len) {
+  assert(len != NULL);
+  assert(mac_addr != NULL);
+  spi_begin(w5100_spi_config);
+  *len = w5100_read_sipr(mac_addr);
   spi_end();
 }
