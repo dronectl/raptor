ce74055b516b4c42fd061c50c6e54fc231caa3a3
ztnel <christian911@sympatico.ca>
2023-01-15 18:51:03 -0500

    add socket register access functions and cmd codes

--- include/w5100.h
+++ include/w5100.h
@@ -22,6 +22,13 @@
 #define W5100_MAX_SOCK_NUM 4
 #endif
 
+// W5100 socket register width in memory
+#define W5100_SOCK_REG_MEM_SIZE 0x100
+// W5100 base socket register address (Socket 0)
+#define W5100_SOCK_BASE 0x0400
+#define W5100_MEMAP_SREG_BASE(n)                                               \
+  (W5100_SOCK_BASE + (W5100_SOCK_REG_MEM_SIZE * n))
+
 /**
  * @brief W5100 API Status Codes
  *
@@ -44,6 +51,75 @@ static const spi_config_t w5100_spi_config = {
 #define MR_AI (uint8_t)(1 << 1)    // Address Auto-Increment in Indirect Bus I/F
 #define MR_IND (uint8_t)(1 << 0)   // Indirect Bus I/F mode
 
+/**
+ * @brief Socket N Interrupt Register
+ * 4.2 Socket Registers
+ */
+#define SNIR_SENDOK (uint8_t)(1 << 4)  // Set if send op completed
+#define SNIR_TIMEOUT (uint8_t)(1 << 3) // Set if timeout occurs
+#define SNIR_RECV (uint8_t)(1 << 2)    // Set when phy receives data
+// set when connection termination is requested or finished
+#define SNIR_DISCON (uint8_t)(1 << 1)
+#define SNIR_CON (uint8_t)(1 << 0) // set if connection established
+
+/**
+ * @brief Socket Channel numbers. Use this enum to perform transactions to
+ * target sockets.
+ *
+ */
+enum W5100SockChannel { CH0 = 0, CH1 = 1, CH2 = 2, CH3 = 3 };
+
+enum W5100Proto {
+  PROTO_CLOSED = 0x0,
+  PROTO_TCP = 0x1,
+  PROTO_UDP = 0x2,
+  PROTO_IPRAW = 0x3,
+  PROTO_MACRAW = 0x4,
+  PROTO_PPPOE = 0x5
+};
+
+enum W5100SockCmds {
+  SOCK_OPEN = 0x01,
+  // TCP Mode Only. Waits for connection request from remote peer (TCP Client).
+  SOCK_LISTEN = 0x02,
+  // TCP Mode Only. Sends connection request to remote peer (TCP Server).
+  SOCK_CONNECT = 0x04,
+  // TCP Mode Only. Sends connecion termination request.
+  SOCK_DISCON = 0x08,
+  SOCK_CLOSE = 0x10,
+  // Transmit data up to size of TX write pointer
+  SOCK_SEND = 0x20,
+  // UDP Mode only. Performs send operation without ARP and uses DHAR
+  SOCK_SEND_MAC = 0x21,
+  // TCP Mode Only. Check connection status by sending 1 byte.
+  SOCK_SEND_KEEP = 0x22,
+  SOCK_RECV = 0x40,
+};
+
+/**
+ * @brief Socket N Status Register States.
+ * Sn_SR (Socket n Status Register) [R] [0x0403, 0x0503, 0x0603, 0x0703] [0x00]
+ */
+enum W5100State {
+  // All connection resources are released.
+  SNSR_CLOSED = 0x00,
+  SNSR_INIT = 0x13,
+  SNSR_LISTEN = 0x14,
+  SNSR_ESTABLISHED = 0x17,
+  SNSR_CLOSE_WAIT = 0x1C,
+  SNSR_UDP = 0x22,
+  SNSR_IPRAW = 0x32,
+  SNSR_MACRAW = 0x42,
+  SNSR_PPPOE = 0x5F,
+  SNSR_SYNSENT = 0x15,
+  SNSR_SYNRECV = 0x16,
+  SNSR_FIN_WAIT = 0x18,
+  SNSR_CLOSING = 0x1A,
+  SNSR_TIME_WAIT = 0x1B,
+  SNSR_LAST_ACK = 0x1D,
+  SNSR_ARP = 0x01
+};
+
 void _read_byte(uint16_t addr, uint8_t *buffer);
 void _write_byte(uint16_t addr, const uint8_t buffer);
 uint16_t _read_bytes(uint16_t addr, uint8_t *buffer, uint16_t len);
@@ -64,23 +140,6 @@ uint16_t _write_bytes(uint16_t addr, const uint8_t *buffer, uint16_t len);
   }
 
 /**
- * @brief Common 16 bit register read and write functions
- *
- */
-#define __CREGISTER16(name, address)                                           \
-  static uint16_t w5100_write_##name(const uint16_t _data) {                   \
-    uint8_t buf[2];                                                            \
-    buf[0] = _data >> 8;                                                       \
-    buf[1] = _data & 0xFF;                                                     \
-    return _write_bytes(address, buf, 2);                                      \
-  }                                                                            \
-  static uint16_t w5100_read_##name(void) {                                    \
-    uint8_t buf[2];                                                            \
-    read_bytes(address, buf, 2);                                               \
-    return (buf[0] << 8) | buf[1];                                             \
-  }
-
-/**
  * @brief Common N-bit register read and write functions
  *
  */
@@ -90,6 +149,34 @@ uint16_t _write_bytes(uint16_t addr, const uint8_t *buffer, uint16_t len);
   }                                                                            \
   static uint16_t w5100_read_##name(uint8_t *_buffer) {                        \
     return _read_bytes(address, _buffer, size);                                \
+  }
+
+/**
+ * @brief Nth Socket 8 bit register read and write functions
+ *
+ */
+#define __SREGISTER8(name, address)                                            \
+  static void w5100_write_sn_##name(enum W5100SockChannel _s, uint8_t _data) { \
+    _write_byte(W5100_MEMAP_SREG_BASE(_s), _data);                             \
+  }                                                                            \
+  static uint8_t w5100_read_sn_##name(enum W5100SockChannel _s) {              \
+    uint8_t buffer;                                                            \
+    _read_byte(W5100_MEMAP_SREG_BASE(_s), &buffer);                            \
+    return buffer;                                                             \
+  }
+
+/**
+ * @brief Nth Socket N-bit register read and write functions
+ *
+ */
+#define __SREGISTER_N(name, address, size)                                     \
+  static uint16_t w5100_write_sn_##name(enum W5100SockChannel _s,              \
+                                        uint8_t *_buff) {                      \
+    return _write_bytes(W5100_MEMAP_SREG_BASE(_s), _buff, size);               \
+  }                                                                            \
+  static uint16_t w5100_read_sn_##name(enum W5100SockChannel _s,               \
+                                       uint8_t *_buff) {                       \
+    return _read_bytes(W5100_MEMAP_SREG_BASE(_s), _buff, size);                \
   }
 
 w5100_status_t w5100_verify_hw(void);
@@ -105,18 +192,44 @@ __CREGISTER_N(gar, 0x0001, 4)  // gateway address register
 __CREGISTER_N(subr, 0x0005, 4) // subnet mask address register
 __CREGISTER_N(shar, 0x0009, 6) // source hardware address register
 __CREGISTER_N(sipr, 0x000F, 4) // source ip address register
-// __CREGISTER8(ir, 0x0015)       // interrupt register
-// __CREGISTER8(imr, 0x0016)      // interrupt mask register
-// __CREGISTER_N(rtr, 0x0017, 2)  // retry time-value register
-// __CREGISTER8(rcr, 0x0019)      // retry count register
-// __CREGISTER8(rmsr, 0x001A)     // rx memory size register
-// __CREGISTER8(tmsr, 0x001B)     // tx memory size register
-// __CREGISTER8(patr, 0x001C)     // authentication type in PPPoE mode
-// __CREGISTER8(ptimer,
-//              0x0028) // PPP link control protocol request timer register
-// __CREGISTER8(pmagic,
-//              0x0029) // PPP link control protocol magic number register
-// __CREGISTER_N(uipr, 0x002A, 4)  // unreachable ip address register (UDP Mode)
-// __CREGISTER_N(uport, 0x002E, 2) // unreachable port register (UDP Mode)
+__CREGISTER8(ir, 0x0015)       // interrupt register
+__CREGISTER8(imr, 0x0016)      // interrupt mask register
+__CREGISTER_N(rtr, 0x0017, 2)  // retry time-value register
+__CREGISTER8(rcr, 0x0019)      // retry count register
+__CREGISTER8(rmsr, 0x001A)     // rx memory size register
+__CREGISTER8(tmsr, 0x001B)     // tx memory size register
+__CREGISTER8(patr, 0x001C)     // authentication type in PPPoE mode
+__CREGISTER8(ptimer,
+             0x0028) // PPP link control protocol request timer register
+__CREGISTER8(pmagic,
+             0x0029) // PPP link control protocol magic number register
+__CREGISTER_N(uipr, 0x002A, 4)  // unreachable ip address register (UDP Mode)
+__CREGISTER_N(uport, 0x002E, 2) // unreachable port register (UDP Mode)
+
+/**
+ * @brief Socket register definitions
+ *
+ * 4.2 Socket Registers
+ */
+__SREGISTER8(mr, 0x0000)         // Mode
+__SREGISTER8(cr, 0x0001)         // Command
+__SREGISTER8(ir, 0x0002)         // Interrupt
+__SREGISTER8(sr, 0x0003)         // Status
+__SREGISTER_N(port, 0x0004, 2)   // Source Port
+__SREGISTER_N(dhar, 0x0006, 6)   // Destination Hardw Addr
+__SREGISTER_N(dipr, 0x000C, 4)   // Destination IP Addr
+__SREGISTER_N(dport, 0x0010, 2)  // Destination Port
+__SREGISTER_N(mssr, 0x0012, 2)   // Max Segment Size
+__SREGISTER8(proto, 0x0014)      // Protocol in IP RAW Mode
+__SREGISTER8(tos, 0x0015)        // IP TOS
+__SREGISTER8(ttl, 0x0016)        // IP TTL
+__SREGISTER8(rx_size, 0x001E)    // RX Memory Size (W5200 only)
+__SREGISTER8(tx_size, 0x001F)    // RX Memory Size (W5200 only)
+__SREGISTER_N(tx_fsr, 0x0020, 2) // TX Free Size
+__SREGISTER_N(tx_rd, 0x0022, 2)  // TX Read Pointer
+__SREGISTER_N(tx_wr, 0x0024, 2)  // TX Write Pointer
+__SREGISTER_N(rx_rsr, 0x0026, 2) // RX Free Size
+__SREGISTER_N(rx_rd, 0x0028, 2)  // RX Read Pointer
+__SREGISTER_N(rx_wr, 0x002A, 2)  // RX Write Pointer (supported?)
 
 #endif // __W5100_H__
