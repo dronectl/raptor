38ec2c445d5662b1dffa0a820a138a5f0dbf4e30
ztnel <christian911@sympatico.ca>
2023-01-16 23:29:12 -0500

    random number generation using ADC sampling

--- src/utils.c
+++ src/utils.c
@@ -1,5 +1,6 @@
 
 #include "utils.h"
+#include "adc.h"
 #include <avr/cpufunc.h>
 
 /**
@@ -11,4 +12,19 @@ void delay_cycles(uint64_t cycles) {
   for (uint64_t i = 0; i < cycles; i++) {
     _NOP();
   };
+}
+
+/**
+ * @brief Generate a random number by sampling the ADC.
+ * NOTE: This should only be used at program startup to avoid interfering with
+ * ADC operation
+ *
+ * @return uint8_t randomly generated number
+ */
+uint8_t generate_random(void) {
+  adc_init();
+  delay_cycles(51);
+  uint8_t rand = adc_sample_lower();
+  adc_shutdown();
+  return rand;
 }
