36ced1064af18ad9827060fe18748394cec70573
ztnel <christian911@sympatico.ca>
2023-01-16 23:29:30 -0500

    use configure function for setting up enet phy

--- src/main.c
+++ src/main.c
@@ -39,31 +39,19 @@ int main(void) {
   gw.bytes[1] = 168;
   gw.bytes[2] = 2;
   gw.bytes[3] = 1;
-  ethernet_set_gateway(gw);
-  ethernet_get_gateway(&gw, &len);
   ipv4_address_t mask;
   mask.bytes[0] = 255;
   mask.bytes[1] = 255;
   mask.bytes[2] = 0;
   mask.bytes[3] = 0;
-  ethernet_set_subnet_mask(mask);
-  ethernet_get_subnet_mask(&mask, &len);
   ipv4_address_t ip_addr;
   ip_addr.bytes[0] = 0;
   ip_addr.bytes[1] = 0;
   ip_addr.bytes[2] = 0;
   ip_addr.bytes[3] = 0;
-  ethernet_set_ip_addr(ip_addr);
-  ethernet_get_gateway(&ip_addr, &len);
-  uint8_t mac_addr[6];
-  mac_addr[0] = 0xFE;
-  mac_addr[1] = 0xDC;
-  mac_addr[2] = 0xBA;
-  mac_addr[3] = 0x98;
-  mac_addr[4] = 0x76;
-  mac_addr[5] = 0x54;
-  ethernet_set_mac_addr((const uint8_t *)mac_addr);
-  ethernet_get_mac_addr(mac_addr, &len);
+  enet_config_t configuration = {
+      .gateway = gw, .subnet_mask = mask, .ip_addr = ip_addr};
+  ethernet_configure(&configuration);
   spinlock();
   return 0;
 }
