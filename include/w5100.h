120c934b28668c41411a8af6411e6e8317317e56
ztnel <christian911@sympatico.ca>
2023-01-20 01:50:56 -0500

    fix bug in socket register addressing

--- include/w5100.h
+++ include/w5100.h
@@ -158,11 +158,11 @@ uint16_t _write_bytes(uint16_t addr, const uint8_t *buffer, uint16_t len);
 #define __SREGISTER8(name, address)                                            \
   static void w5100_write_sn_##name(const enum W5100SCH _s,                    \
                                     const uint8_t _data) {                     \
-    _write_byte(W5100_MEMAP_SREG_BASE(_s), _data);                             \
+    _write_byte(W5100_MEMAP_SREG_BASE(_s) + address, _data);                   \
   }                                                                            \
   static uint8_t w5100_read_sn_##name(enum W5100SCH _s) {                      \
     uint8_t buffer;                                                            \
-    _read_byte(W5100_MEMAP_SREG_BASE(_s), &buffer);                            \
+    _read_byte(W5100_MEMAP_SREG_BASE(_s) + address, &buffer);                  \
     return buffer;                                                             \
   }
 
@@ -173,10 +173,10 @@ uint16_t _write_bytes(uint16_t addr, const uint8_t *buffer, uint16_t len);
 #define __SREGISTER_N(name, address, size)                                     \
   static uint16_t w5100_write_sn_##name(const enum W5100SCH _s,                \
                                         const uint8_t *_buff) {                \
-    return _write_bytes(W5100_MEMAP_SREG_BASE(_s), _buff, size);               \
+    return _write_bytes(W5100_MEMAP_SREG_BASE(_s) + address, _buff, size);     \
   }                                                                            \
   static uint16_t w5100_read_sn_##name(enum W5100SCH _s, uint8_t *_buff) {     \
-    return _read_bytes(W5100_MEMAP_SREG_BASE(_s), _buff, size);                \
+    return _read_bytes(W5100_MEMAP_SREG_BASE(_s) + address, _buff, size);      \
   }
 
 w5100_status_t w5100_verify_hw(void);
