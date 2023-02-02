48050439a2058e9d3c8dada27bdd903e8896311f
ztnel <christian911@sympatico.ca>
2023-02-02 01:48:34 -0500

    testing logging macros

--- src/main.c
+++ src/main.c
@@ -9,8 +9,10 @@
  *
  */
 
-#include "ethernet.h"
+// #include "ethernet.h"
+#include "logger.h"
 #include "spi.h"
+#include "usart.h"
 #include "utils.h"
 #include <avr/cpufunc.h>
 #include <avr/io.h>
@@ -23,34 +25,43 @@ static void spinlock(void) {
   DDRB = (1 << DDB5);
   // sync nop
   _NOP();
+  critical("Entering spinlock.");
   while (1) {
+    logger_flush();
     // blink SCK
     PORTB ^= (1 << PORTB5);
     delay_cycles(100000);
+    info("info log test");
+    trace("trace log test");
+    warning("warning log test");
+    error("error log test");
+    critical("critical log test");
   }
 }
 
 int main(void) {
+  usart_init();
+  logger_init();
   // initialize phy
-  ethernet_phy_init();
-  ipv4_address_t gw;
-  gw.bytes[0] = 192;
-  gw.bytes[1] = 168;
-  gw.bytes[2] = 2;
-  gw.bytes[3] = 1;
-  ipv4_address_t mask;
-  mask.bytes[0] = 255;
-  mask.bytes[1] = 255;
-  mask.bytes[2] = 0;
-  mask.bytes[3] = 0;
-  ipv4_address_t ip_addr;
-  ip_addr.bytes[0] = 0;
-  ip_addr.bytes[1] = 0;
-  ip_addr.bytes[2] = 0;
-  ip_addr.bytes[3] = 0;
-  enet_config_t configuration = {
-      .gateway = gw, .subnet_mask = mask, .ip_addr = ip_addr};
-  ethernet_configure(&configuration);
+  // ethernet_phy_init();
+  // ipv4_address_t gw;
+  // gw.bytes[0] = 192;
+  // gw.bytes[1] = 168;
+  // gw.bytes[2] = 2;
+  // gw.bytes[3] = 1;
+  // ipv4_address_t mask;
+  // mask.bytes[0] = 255;
+  // mask.bytes[1] = 255;
+  // mask.bytes[2] = 0;
+  // mask.bytes[3] = 0;
+  // ipv4_address_t ip_addr;
+  // ip_addr.bytes[0] = 0;
+  // ip_addr.bytes[1] = 0;
+  // ip_addr.bytes[2] = 0;
+  // ip_addr.bytes[3] = 0;
+  // enet_config_t configuration = {
+  //     .gateway = gw, .subnet_mask = mask, .ip_addr = ip_addr};
+  // ethernet_configure(&configuration);
   spinlock();
   return 0;
 }
