2a67658ec49c7baa58eaf13a446872b44ad325ee
ztnel <christian911@sympatico.ca>
2023-02-10 01:47:47 -0500

    hardcode system clock

--- include/FreeRTOSConfig.h
+++ include/FreeRTOSConfig.h
@@ -78,7 +78,7 @@
 #define configUSE_PREEMPTION 1
 #define configUSE_IDLE_HOOK 1
 #define configUSE_TICK_HOOK 1
-#define configCPU_CLOCK_HZ (SystemCoreClock)
+#define configCPU_CLOCK_HZ (16000000)
 #define configTICK_RATE_HZ ((TickType_t)1000)
 #define configMAX_PRIORITIES (5)
 #define configMINIMAL_STACK_SIZE ((unsigned short)130)
