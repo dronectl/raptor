7835d84812bbb1c2ab85ac9f2696b7c562209d79
ztnel <christian911@sympatico.ca>
2023-02-26 00:45:43 -0500

    add module for disovering opeocd

---
+++ cmake/modules/openocd.cmake
@@ -0,0 +1,17 @@
+cmake_minimum_required(VERSION 3.22)
+function (find_openocd)
+  # Find the host's openocd program
+  message(STATUS "Finding OpenOCD binary")
+  find_program(
+    OPENOCD_EXECUTABLE
+    NAMES openocd
+    PATHS "/usr/local/bin"
+          "/usr/bin"
+          "$ENV{PROGRAMFILES}/OpenOCD"
+    DOC "Path to the openocd program"
+  )
+  # Check if the program was found
+  if (NOT OPENOCD_EXECUTABLE)
+    message(FATAL_ERROR "Could not find openocd program")
+  endif()
+endfunction()
