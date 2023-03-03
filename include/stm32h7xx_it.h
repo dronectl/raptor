b13cd31f052540e94a1078280e0e8262689023fb
ztnel <christian911@sympatico.ca>
2023-03-03 01:00:36 -0500

    clang fmt

--- include/stm32h7xx_it.h
+++ include/stm32h7xx_it.h
@@ -1,28 +1,24 @@
 /**
-  ******************************************************************************
-  * @file    LwIP/LwIP_TCP_Echo_Server/Inc/stm32h7xx_it.h
-  * @author  MCD Application Team
-  * @brief   This file contains the headers of the interrupt handlers.
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2017 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file    LwIP/LwIP_TCP_Echo_Server/Inc/stm32h7xx_it.h
+ * @author  MCD Application Team
+ * @brief   This file contains the headers of the interrupt handlers.
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2017 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __STM32H7xx_IT_H
 #define __STM32H7xx_IT_H
-
-#ifdef __cplusplus
- extern "C" {
-#endif
 
 /* Includes ------------------------------------------------------------------*/
 /* Exported types ------------------------------------------------------------*/
@@ -40,10 +36,4 @@ void DebugMon_Handler(void);
 void PendSV_Handler(void);
 void SysTick_Handler(void);
 
-#ifdef __cplusplus
-}
-#endif
-
 #endif /* __STM32H7xx_IT_H */
-
-
