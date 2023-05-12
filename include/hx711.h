3c1540a9f3ccd7cab6da1f61ee5043acd427cf13
ztnel <christian@leapsystems.online>
2023-05-11 21:36:42 -0400

    first pass of Hx711 driver

--- include/hx711.h
+++ include/hx711.h
@@ -5,19 +5,16 @@
 #define MAX 0x7FFFF
 
 typedef struct hx711_config_t {
-  uint8_t sck;
-  uint8_t sda;
+  // uint8_t sck;
+  // uint8_t sda;
   uint8_t gain;
   uint16_t offset;
-  uint16_t scale;
+  uint16_t slope;
 
 } hx711_config_t;
 
-void hx711_init(hx711_config_t *hc);
-void hx711_deinit(void);
-
-/**
- * @brief 
- * 
- */
+void hx711_init(void);
 uint32_t hx711_read(void);
+void hx711_task(void *pv_params);
+void hx711_shutdown(void);
+void hx711_boot(void);
