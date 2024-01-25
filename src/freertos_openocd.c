e3bc2c8b14ccc251ed6a0f78f4bd458ac2261dbc
ztnel <christian911@sympatico.ca>
2024-01-25 16:19:01 -0500

    openocd RTOS notes

--- src/freertos_openocd.c
+++ src/freertos_openocd.c
@@ -1,3 +1,14 @@
+/**
+ * @file freertos_openocd.c
+ * @author ztnel (christian911@sympatico.ca)
+ * @brief This file enables FreeRTOS task stats collection by OpenOCD. This is only required for
+ * DEBUG builds and can be removed for production releases
+ * @version 0.1
+ * @date 2024-01
+ *
+ * @copyright Copyright © 2024 dronectl
+ *
+ */
 #include "FreeRTOS.h"
 
 #ifdef __GNUC__
