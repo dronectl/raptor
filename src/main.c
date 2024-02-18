7982683a76b7784ac91aa70243776dbdeb0390cf
ztnel <christian911@sympatico.ca>
2024-02-17 19:26:41 -0500

    remove freertos cbs

--- src/main.c
+++ src/main.c
@@ -26,99 +26,6 @@ static void bsp_config(void);
 static void mpu_config(void);
 static void cpu_cache_enable(void);
 
-#ifdef RAPTOR_DEBUG
-/**
- * @brief FreeRTOS task CPU usage statistics timer configuration.  Only used in
- * debug builds. TIM2 peripheral is used arbitrarily.
- *
- */
-void vconfigure_rtos_stats_timer(void) {
-  // Enable the clock for Timer 2
-  RCC->APB1HENR |= RCC_APB1HENR_TIM23EN;
-  // Set the prescaler for Timer 2
-  TIM23->PSC = SystemCoreClock / 10000 - 1;
-  // Configure Timer 2 in up-counting mode
-  TIM23->CR1 &= ~TIM_CR1_DIR; // Up-counting mode
-  TIM23->CR1 &= ~TIM_CR1_CMS; // Edge-aligned mode
-  // Enable Timer 2
-  TIM23->CR1 |= TIM_CR1_CEN;
-}
-/**
- * @brief FreeRTOS task CPU usage statistics helper. Fetches current counter
- * value.
- *
- * @return uint32_t timer2 counter register
- */
-uint32_t vget_runtime_count(void) { return TIM23->CNT; }
-#endif // RAPTOR_DEBUG
-
-/**
- * @brief This is to provide the memory that is used by the RTOS daemon/time
- * task.
- *
- * If configUSE_STATIC_ALLOCATION is set to 1, so the application must provide
- * an implementation of vApplicationGetTimerTaskMemory() to provide the memory
- * that is used by the RTOS daemon/time task.
- */
-void vApplicationGetTimerTaskMemory(StaticTask_t **ppxTimerTaskTCBBuffer,
-                                    StackType_t **ppxTimerTaskStackBuffer,
-                                    uint32_t *pulTimerTaskStackSize) {
-  /* If the buffers to be provided to the Timer task are declared inside this
-   * function then they must be declared static - otherwise they will be
-   * allocated on the stack and so not exists after this function exits. */
-  static StaticTask_t xTimerTaskTCB;
-  static StackType_t uxTimerTaskStack[configTIMER_TASK_STACK_DEPTH];
-
-  /* Pass out a pointer to the StaticTask_t structure in which the Idle
-   * task's state will be stored. */
-  *ppxTimerTaskTCBBuffer = &xTimerTaskTCB;
-
-  /* Pass out the array that will be used as the Timer task's stack. */
-  *ppxTimerTaskStackBuffer = uxTimerTaskStack;
-
-  /* Pass out the size of the array pointed to by *ppxTimerTaskStackBuffer.
-   * Note that, as the array is necessarily of type StackType_t,
-   * configMINIMAL_STACK_SIZE is specified in words, not bytes. */
-  *pulTimerTaskStackSize = configTIMER_TASK_STACK_DEPTH;
-}
-
-/* configUSE_STATIC_ALLOCATION is set to 1, so the application must provide an
- * implementation of vApplicationGetIdleTaskMemory() to provide the memory that
- * is used by the Idle task. */
-void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer,
-                                   StackType_t **ppxIdleTaskStackBuffer,
-                                   uint32_t *pulIdleTaskStackSize) {
-  /* If the buffers to be provided to the Idle task are declared inside this
-   * function then they must be declared static - otherwise they will be
-   * allocated on the stack and so not exists after this function exits. */
-  static StaticTask_t xIdleTaskTCB;
-  static StackType_t uxIdleTaskStack[configMINIMAL_STACK_SIZE];
-
-  /* Pass out a pointer to the StaticTask_t structure in which the Idle
-   * task's state will be stored. */
-  *ppxIdleTaskTCBBuffer = &xIdleTaskTCB;
-
-  /* Pass out the array that will be used as the Idle task's stack. */
-  *ppxIdleTaskStackBuffer = uxIdleTaskStack;
-
-  /* Pass out the size of the array pointed to by *ppxIdleTaskStackBuffer.
-   * Note that, as the array is necessarily of type StackType_t,
-   * configMINIMAL_STACK_SIZE is specified in words, not bytes. */
-  *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
-}
-
-void vApplicationStackOverflowHook(TaskHandle_t pxTask, char *pcTaskName) {
-  (void)pcTaskName;
-  (void)pxTask;
-
-  /* Run time stack overflow checking is performed if
-  configCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
-  function is called if a stack overflow is detected. */
-  taskDISABLE_INTERRUPTS();
-  for (;;)
-    ;
-}
-
 int main(void) {
   /* Configure the MPU attributes as Device memory for ETH DMA descriptors */
   mpu_config();
@@ -126,11 +33,6 @@ int main(void) {
   /* Enable the CPU Cache */
   cpu_cache_enable();
 
-  /* STM32H7xx HAL library initialization:
-       - Configure the SysTick to generate an interrupt each 1 msec
-       - Set NVIC Group Priority to 4
-       - Low Level Initialization
-     */
   HAL_Init();
 
   /* Configure the system clock to 520 MHz */
