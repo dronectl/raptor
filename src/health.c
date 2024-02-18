0f3c7132f09e0c9eecd6743ad0eec007cd40427c
ztnel <christian911@sympatico.ca>
2024-02-17 21:53:32 -0500

    test

--- src/health.c
+++ src/health.c
@@ -1,7 +1,7 @@
-
 #include "health.h"
 #include "FreeRTOS.h"
 #include "bme280.h"
+#include "logger.h"
 #include "stm32h7xx_hal.h"
 #include "task.h"
 
@@ -80,12 +80,15 @@ static enum HealthState fsm_tick(const enum HealthState state) {
 }
 
 void health_main(void *pv_params) {
+  uint32_t ticks;
   const TickType_t delay = 100 / portTICK_PERIOD_MS;
   enum HealthState state = HEALTH_INIT;
   // get i2c2 handle and set bme280
   I2C_HandleTypeDef hi2c2 = *(I2C_HandleTypeDef *)pv_params;
   bme280.i2c = hi2c2;
   while (1) {
+    ticks = HAL_GetTick();
+    trace("[%u] Hello from state: %d", ticks, state);
     state = fsm_tick(state);
     vTaskDelay(delay);
   }
