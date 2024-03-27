c6882816cd7c705a6681a4a9ec9382ee21298100
ztnel <christian911@sympatico.ca>
2024-03-27 00:27:08 -0400

    utest set access, init and reset

--- tests/unit/test_sysreg.cc
+++ tests/unit/test_sysreg.cc
@@ -4,6 +4,29 @@ extern "C"{
 #include "sysreg.h"
 }
 
+TEST(SysRegTest, SysRegInit) {
+  EXPECT_EQ(sysreg_init(), SYSREG_OK) << "init returned non-zero status code";
+  uint8_t sys_stat;
+  sysreg_get_u8(SYSREG_SYS_STAT, &sys_stat);
+  EXPECT_EQ(sys_stat, SYSREG_SYS_STAT_RESET) << "failed to reset SYS_STAT";
+  uint32_t uuid;
+  sysreg_get_u32(SYSREG_UUID, &uuid);
+  EXPECT_EQ(uuid, SYSREG_UUID_RESET) << "failed to reset UUID";
+  uint32_t hw_version;
+  sysreg_get_u32(SYSREG_HW_VERSION, &hw_version);
+  EXPECT_EQ(hw_version, SYSREG_HW_VERSION_RESET) << "failed to reset HW_VERSION";
+  uint32_t fw_version;
+  sysreg_get_u32(SYSREG_FW_VERSION, &fw_version);
+  EXPECT_EQ(fw_version, SYSREG_FW_VERSION_RESET) << "failed to reset FW_VERSION";
+}
+
+TEST(SysRegTest, SysRegSetAccess) {
+  EXPECT_EQ(sysreg_set_access(SYSREG_GPU8, SYSREG_ACCESS_L), SYSREG_OP_ERR) << "lock bit set did not return SYSREG_OP_ERR";
+  EXPECT_EQ(sysreg_set_access(999999, SYSREG_ACCESS_R), SYSREG_NOT_FOUND_ERR) << "invalid offset did not return SYSREG_NOT_FOUND_ERR";
+  EXPECT_EQ(sysreg_set_access(SYSREG_GPU8, SYSREG_ACCESS_R), SYSREG_ACCESS_ERR) << "locked register access modification did not return SYSREG_ACCESS_ERR";
+  EXPECT_EQ(sysreg_set_access(SYSREG_GPU8_UL, SYSREG_ACCESS_R), SYSREG_OK) << "unlocked register access modification did not return SYSREG_OK";
+}
+
 TEST(SysRegTest, SysRegU8) {
   const uint8_t data = 255;
   uint8_t buffer;
