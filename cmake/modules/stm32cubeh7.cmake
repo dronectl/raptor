48bc92db4e1a12e322f39d82b84c1a73eca55432
ztnel <christian911@sympatico.ca>
2024-04-14 13:19:04 -0400

    filter out templates in hal files and upgrade cubeh7 version

--- cmake/modules/stm32cubeh7.cmake
+++ cmake/modules/stm32cubeh7.cmake
@@ -4,7 +4,7 @@ message(STATUS "Fetching STM32H7 CMSIS HAL and middleware dependancies")
 FetchContent_Declare(
   stm32h7
   GIT_REPOSITORY https://github.com/STMicroelectronics/STM32CubeH7.git
-  GIT_TAG v1.11.1
+  GIT_TAG v1.11.2
   GIT_PROGRESS TRUE
 )
 FetchContent_MakeAvailable(stm32h7)
@@ -13,9 +13,8 @@ message(STATUS "Exporting FreeRTOS Src and Includes")
 set(
   CMSIS_INCLUDE
   ${stm32h7_SOURCE_DIR}/Drivers/CMSIS/Device/ST/STM32H7xx/Include
-  ${stm32h7_SOURCE_DIR}/Drivers/CMSIS/Core/Include
-  ${stm32h7_SOURCE_DIR}/Drivers/CMSIS/RTOS2/Include
   ${stm32h7_SOURCE_DIR}/Drivers/CMSIS/Include
+  ${stm32h7_SOURCE_DIR}/Drivers/CMSIS/RTOS2/Include
   CACHE INTERNAL "CMSIS_INCLUDE"
 )
 
@@ -67,9 +66,17 @@ set(
 
 message(STATUS "Exporting HAL Src and Includes")
 aux_source_directory(${stm32h7_SOURCE_DIR}/Drivers/STM32H7xx_HAL_Driver/Src HAL_SRCS_TMP)
+# Iterate over each source file in HAL_SRCS
+foreach(src ${HAL_SRCS_TMP})
+    string(FIND ${src} "template" HAS_TEMPLATE)
+    if(${HAS_TEMPLATE} EQUAL -1)
+        list(APPEND FILTERED_SRCS ${src})
+    endif()
+endforeach()
+
 set(
   HAL_SRCS
-  ${HAL_SRCS_TMP}
+  ${FILTERED_SRCS}
   ${stm32h7_SOURCE_DIR}/Drivers/BSP/Components/lan8742/lan8742.c
   ${stm32h7_SOURCE_DIR}/Drivers/BSP/STM32H7xx_Nucleo/stm32h7xx_nucleo.c
   CACHE INTERNAL "HAL_SRCS"
