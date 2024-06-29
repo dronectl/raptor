69b6f88bb29fc088ac226dd08b7b307374303a56
ztnel <christian911@sympatico.ca>
2024-06-29 18:31:44 -0400

    implement static memory allocation for scpi task (large ram requirement)

--- core/lib/scpi/server.c
+++ core/lib/scpi/server.c
@@ -19,8 +19,11 @@
 #include <FreeRTOS.h>
 #include <task.h>
 
-//__attribute__((section(".ram_d3"))) static StackType_t scpi_stk[STACK_SIZE];
-//__attribute__((section(".ram_d3"))) static StaticTask_t scpi_task_buffer;
+#define STACK_SIZE 4000
+
+__attribute__((section(".ram_d3"))) static StackType_t scpi_task_stk[STACK_SIZE];
+__attribute__((section(".ram_d3"))) static StaticTask_t scpi_task_buffer;
+static TaskHandle_t scpi_task_handle;
 
 static void handle_scpi_request(const struct scpi_handle *shandle) {
   char buffer[SCPI_MAX_INPUT_BUFFER_LEN] = {0};
@@ -35,14 +38,13 @@ static void handle_scpi_request(const struct scpi_handle *shandle) {
   }
   parser_run(&phandle, &lhandle);
   for (int i = 0; i < phandle.cmdidx + 1; i++) {
-    info("S:0x%X|Eti:%d|Eci:%d|Es:0x%X|H:%d|A:%d\n", phandle.commands[i].spec, phandle.error.tidx, phandle.error.cidx, phandle.error, phandle.commands[i].hidx + 1, phandle.commands[i].aidx + 1);
+    info("S:0x%X|Eti:%d|Eci:%d|Es:0x%X|H:%d|A:%d\n", phandle.commands[i].spec, phandle.error.tidx, phandle.error.cidx, phandle.error.code, phandle.commands[i].hidx + 1, phandle.commands[i].aidx + 1);
     int index = commands_search_index(phandle.commands[i].headers, phandle.commands[i].hidx);
     if (index < 0) {
-      error("command index %d endpoint not found\n", i);
-      continue;
+      warning("command endpoint not found\n");
     }
     if (phandle.commands[i].spec & PARSER_CMD_SPEC_QUERY) {
-      commands_process_query(index, phandle.commands[i].aidx + 1, phandle.commands[i].args, &buffer[0], sizeof(buffer));
+      commands_process_query(index, phandle.commands[i].aidx + 1, phandle.commands[i].args, &buffer[i], sizeof(buffer));
     } else if (phandle.commands[i].spec & PARSER_CMD_SPEC_SET) {
       commands_process_write(index, phandle.commands[i].aidx + 1, phandle.commands[i].args);
     }
@@ -55,7 +57,6 @@ static void handle_client_session(int client_fd) {
   shandle.clfd = client_fd;
   for (;;) {
     memset(shandle.buffer, '\0', SCPI_MAX_INPUT_BUFFER_LEN);
-    info("Waiting on read...\n");
     shandle.buflen = read(client_fd, shandle.buffer, sizeof(shandle.buffer));
     if (shandle.buflen <= 0) {
       break;
@@ -86,11 +87,11 @@ static void scpi_main(__attribute__((unused)) void *argument) {
   while (1) {
     info("blocking on accept...\n");
     client_fd = accept(sock, (struct sockaddr *)&remotehost, (socklen_t *)&size);
-    info("Accepted connection with fd: %d\n", client_fd);
     if (client_fd < 0) {
       taskYIELD();
       continue;
     }
+    info("Accepted connection with fd: %d\n", client_fd);
     handle_client_session(client_fd);
     info("closing session\n");
     close(client_fd);
@@ -101,9 +102,8 @@ error:
 }
 
 system_status_t scpi_init(void) {
-  TaskHandle_t scpi_handle = NULL;
-  BaseType_t ret = xTaskCreate(scpi_main, "scpi_task", configMINIMAL_STACK_SIZE, NULL, 11, &scpi_handle);
-  if (ret != pdPASS) {
+  scpi_task_handle = xTaskCreateStatic(scpi_main, "scpi_task", STACK_SIZE, NULL, tskIDLE_PRIORITY + 10, scpi_task_stk, &scpi_task_buffer);
+  if (scpi_task_handle == NULL) {
     return SYSTEM_MOD_FAIL;
   }
   return SYSTEM_OK;
