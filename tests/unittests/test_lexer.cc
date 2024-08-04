43c454e854300594a32f635760552d8f7dd80769
ztnel <christian911@sympatico.ca>
2024-08-04 09:13:57 -0400

    update lexer test to reflect name changes

--- tests/unittests/test_lexer.cc
+++ tests/unittests/test_lexer.cc
@@ -6,44 +6,45 @@ extern "C" {
 
 TEST(SCPILexerTest, SCPILexerInit) {
   // clang-format off
-  struct lexer_handle lhandle = {
-    .cidx = 4,
-    .tidx = 3,
-    .status = 4,
-    .prev_tt = LEXER_TT_SPACE,
+  struct lexer_handle lexer = {
+    .char_index = 4,
+    .token_index = 3,
+    .status_flags = 4,
+    .error_flags = 4,
+    .prev_token_type = LEXER_TT_SPACE,
     .tokens={
       {
         .type=LEXER_TT_SPACE,
         .token={
           .len = 2,
-          .token=""
+          .value=""
         }
       },
       {
         .type=LEXER_TT_TOKEN,
         .token={
           .len = 5,
-          .token="dsala"
+          .value="dsala"
         }
       }
     }
   };
   // clang-format on
-  lexer_init(&lhandle);
+  lexer_init(&lexer);
   for (int i = 0; i < SCPI_MAX_TOKENS; i++) {
-    EXPECT_EQ(lhandle.tokens[i].token.len, 0) << "token length not cleared at index" << i;
+    EXPECT_EQ(lexer.tokens[i].token.len, 0) << "token length not cleared at index" << i;
     for (int j = 0; j < SCPI_MAX_TOKEN_LEN; j++) {
-      EXPECT_EQ(lhandle.tokens[i].token.token[j], '\0') << "token not set to \0 at index" << j;
+      EXPECT_EQ(lexer.tokens[i].token.value[j], '\0') << "token not set to \0 at index" << j;
     }
-    EXPECT_EQ(lhandle.tokens[i].type, LEXER_TT_NULL) << "token type not set to LEXER_TT_NULL for index" << i;
+    EXPECT_EQ(lexer.tokens[i].type, LEXER_TT_NULL) << "token type not set to LEXER_TT_NULL for index" << i;
   }
-  EXPECT_EQ(lhandle.status, 0) << "status not cleared";
-  EXPECT_EQ(lhandle.cidx, 0) << "character index not cleared";
-  EXPECT_EQ(lhandle.tidx, 0) << "token index not cleared";
-  EXPECT_EQ(lhandle.err, 0) << "status not cleared";
-  EXPECT_EQ(lhandle.prev_tt, LEXER_TT_NULL) << "previous token type not cleared";
+  EXPECT_EQ(lexer.status_flags, 0) << "status not cleared";
+  EXPECT_EQ(lexer.char_index, 0) << "character index not cleared";
+  EXPECT_EQ(lexer.token_index, 0) << "token index not cleared";
+  EXPECT_EQ(lexer.error_flags, 0) << "status not cleared";
+  EXPECT_EQ(lexer.prev_token_type, LEXER_TT_NULL) << "previous token type not cleared";
 }
 
-TEST(SCPILexerTest, SCPILexerBasic) {
-  struct lexer_handle lhandle = {.cidx = 4, .tidx = 3, .status = 4, .prev_tt = LEXER_TT_SPACE};
+TEST(SCPILexerTest, SCPILexerProcessBasic) {
 }
+TEST(SCPILexerTest, SCPILexerInit) {
