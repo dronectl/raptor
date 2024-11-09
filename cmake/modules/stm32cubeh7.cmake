c5f0c9501b69c21955af3b18afe5a768c7685874
ztnel <christian911@sympatico.ca>
2024-11-09 12:09:58 -0500

    replace slow FetchContent with depth=1 git clone proc pointing to a custom repo with stm32 sources

--- cmake/modules/stm32cubeh7.cmake
+++ cmake/modules/stm32cubeh7.cmake
@@ -1,42 +1,63 @@
 cmake_minimum_required(VERSION 3.14)
-include(FetchContent)
+# Check if Git is available
+find_program(GIT_EXECUTABLE git)
+
+if(NOT GIT_EXECUTABLE)
+    message(FATAL_ERROR "Git not found. Please install Git to proceed.")
+else()
+    message(STATUS "Git found: ${GIT_EXECUTABLE}")
+endif()
+
+# Path where you want to clone the library
+set(STM32H7_SOURCE_DIR "${CMAKE_BINARY_DIR}/external/STM32CubeH7")
+
+function(clone_repo_if_missing url dir)
+    if(NOT EXISTS "${dir}")
+        message(STATUS "Cloning ${url} into ${dir}")
+        execute_process(
+            COMMAND git clone --depth 1 --recurse-submodules ${url} ${dir}
+            WORKING_DIRECTORY "${CMAKE_BINARY_DIR}"
+            RESULT_VARIABLE GIT_CLONE_RESULT
+        )
+        if(NOT GIT_CLONE_RESULT EQUAL 0)
+            message(FATAL_ERROR "Failed to clone repository: ${url}")
+        endif()
+    else()
+        message(STATUS "Repository already cloned: ${dir}")
+    endif()
+endfunction()
+
 message(STATUS "Fetching STM32H7 CMSIS HAL and middleware dependancies")
-FetchContent_Declare(
-  stm32h7
-  GIT_REPOSITORY https://github.com/STMicroelectronics/STM32CubeH7.git
-  GIT_TAG v1.11.2
-  GIT_PROGRESS TRUE
-)
-FetchContent_MakeAvailable(stm32h7)
+clone_repo_if_missing("https://github.com/dronectl/STM32CubeH7.git" "${STM32H7_SOURCE_DIR}")
 
 message(STATUS "Exporting FreeRTOS Src and Includes")
 set(
   CMSIS_INCLUDE
-  ${stm32h7_SOURCE_DIR}/Drivers/CMSIS/Device/ST/STM32H7xx/Include
-  ${stm32h7_SOURCE_DIR}/Drivers/CMSIS/Include
-  ${stm32h7_SOURCE_DIR}/Drivers/CMSIS/Core/Include
+  ${STM32H7_SOURCE_DIR}/Drivers/CMSIS/Device/ST/STM32H7xx/Include
+  ${STM32H7_SOURCE_DIR}/Drivers/CMSIS/Include
+  ${STM32H7_SOURCE_DIR}/Drivers/CMSIS/Core/Include
   CACHE INTERNAL "CMSIS_INCLUDE"
 )
 
 set(
   FREERTOS_SRCS
-  ${stm32h7_SOURCE_DIR}/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.c
-  ${stm32h7_SOURCE_DIR}/Middlewares/Third_Party/FreeRTOS/Source/croutine.c
-  ${stm32h7_SOURCE_DIR}/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c
-  ${stm32h7_SOURCE_DIR}/Middlewares/Third_Party/FreeRTOS/Source/list.c
-  ${stm32h7_SOURCE_DIR}/Middlewares/Third_Party/FreeRTOS/Source/queue.c
-  ${stm32h7_SOURCE_DIR}/Middlewares/Third_Party/FreeRTOS/Source/tasks.c
-  ${stm32h7_SOURCE_DIR}/Middlewares/Third_Party/FreeRTOS/Source/timers.c
-  ${stm32h7_SOURCE_DIR}/Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c
-  ${stm32h7_SOURCE_DIR}/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/port.c
-  ${stm32h7_SOURCE_DIR}/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c
+  ${STM32H7_SOURCE_DIR}/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.c
+  ${STM32H7_SOURCE_DIR}/Middlewares/Third_Party/FreeRTOS/Source/croutine.c
+  ${STM32H7_SOURCE_DIR}/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c
+  ${STM32H7_SOURCE_DIR}/Middlewares/Third_Party/FreeRTOS/Source/list.c
+  ${STM32H7_SOURCE_DIR}/Middlewares/Third_Party/FreeRTOS/Source/queue.c
+  ${STM32H7_SOURCE_DIR}/Middlewares/Third_Party/FreeRTOS/Source/tasks.c
+  ${STM32H7_SOURCE_DIR}/Middlewares/Third_Party/FreeRTOS/Source/timers.c
+  ${STM32H7_SOURCE_DIR}/Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c
+  ${STM32H7_SOURCE_DIR}/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/port.c
+  ${STM32H7_SOURCE_DIR}/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c
   CACHE INTERNAL "FREERTOS_SRCS"
 )
 set(
   FREERTOS_INC
-  ${stm32h7_SOURCE_DIR}/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2
-  ${stm32h7_SOURCE_DIR}/Middlewares/Third_Party/FreeRTOS/Source/include
-  ${stm32h7_SOURCE_DIR}/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/
+  ${STM32H7_SOURCE_DIR}/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2
+  ${STM32H7_SOURCE_DIR}/Middlewares/Third_Party/FreeRTOS/Source/include
+  ${STM32H7_SOURCE_DIR}/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/
   ${CMSIS_INCLUDE}
   CACHE INTERNAL "FREERTOS_INC"
 )
@@ -46,10 +67,10 @@ file(
   GLOB_RECURSE 
   LWIP_SRCS_TMP
   CONFIGURE_DEPENDS
-  "${stm32h7_SOURCE_DIR}/Middlewares/Third_Party/LwIP/src/core/*.c"
-  "${stm32h7_SOURCE_DIR}/Middlewares/Third_Party/LwIP/src/api/*.c"
-  "${stm32h7_SOURCE_DIR}/Middlewares/Third_Party/LwIP/src/netif/ethernet.c"
-  "${stm32h7_SOURCE_DIR}/Middlewares/Third_Party/LwIP/system/OS/sys_arch.c"
+  "${STM32H7_SOURCE_DIR}/Middlewares/Third_Party/LwIP/src/core/*.c"
+  "${STM32H7_SOURCE_DIR}/Middlewares/Third_Party/LwIP/src/api/*.c"
+  "${STM32H7_SOURCE_DIR}/Middlewares/Third_Party/LwIP/src/netif/ethernet.c"
+  "${STM32H7_SOURCE_DIR}/Middlewares/Third_Party/LwIP/system/OS/sys_arch.c"
 )
 
 set(
@@ -59,13 +80,13 @@ set(
 )
 set(
   LWIP_INC
-  ${stm32h7_SOURCE_DIR}/Middlewares/Third_Party/LwIP/src/include
-  ${stm32h7_SOURCE_DIR}/Middlewares/Third_Party/LwIP/system
+  ${STM32H7_SOURCE_DIR}/Middlewares/Third_Party/LwIP/src/include
+  ${STM32H7_SOURCE_DIR}/Middlewares/Third_Party/LwIP/system
   CACHE INTERNAL "LWIP_INC"
 )
 
 message(STATUS "Exporting HAL Src and Includes")
-aux_source_directory(${stm32h7_SOURCE_DIR}/Drivers/STM32H7xx_HAL_Driver/Src HAL_SRCS_TMP)
+aux_source_directory(${STM32H7_SOURCE_DIR}/Drivers/STM32H7xx_HAL_Driver/Src HAL_SRCS_TMP)
 # Iterate over each source file in HAL_SRCS
 foreach(src ${HAL_SRCS_TMP})
     string(FIND ${src} "template" HAS_TEMPLATE)
@@ -77,15 +98,15 @@ endforeach()
 set(
   HAL_SRCS
   ${FILTERED_SRCS}
-  ${stm32h7_SOURCE_DIR}/Drivers/BSP/Components/lan8742/lan8742.c
-  ${stm32h7_SOURCE_DIR}/Drivers/BSP/STM32H7xx_Nucleo/stm32h7xx_nucleo.c
+  ${STM32H7_SOURCE_DIR}/Drivers/BSP/Components/lan8742/lan8742.c
+  ${STM32H7_SOURCE_DIR}/Drivers/BSP/STM32H7xx_Nucleo/stm32h7xx_nucleo.c
   CACHE INTERNAL "HAL_SRCS"
 )
 set(
   HAL_INC
-  ${stm32h7_SOURCE_DIR}/Drivers/BSP/Components/lan8742
-  ${stm32h7_SOURCE_DIR}/Drivers/BSP/STM32H7xx_Nucleo
-  ${stm32h7_SOURCE_DIR}/Drivers/STM32H7xx_HAL_Driver/Inc
+  ${STM32H7_SOURCE_DIR}/Drivers/BSP/Components/lan8742
+  ${STM32H7_SOURCE_DIR}/Drivers/BSP/STM32H7xx_Nucleo
+  ${STM32H7_SOURCE_DIR}/Drivers/STM32H7xx_HAL_Driver/Inc
   ${CMSIS_INCLUDE}
   CACHE INTERNAL "HAL_INC"
 )
