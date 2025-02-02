17af317105234ebc79dbb67709c2e6ac056755c4
ztnel <christian911@sympatico.ca>
2025-02-02 00:13:06 -0500

    common utilities

---
+++ src/common/common.h
@@ -0,0 +1,18 @@
+/**
+ * @file common.h
+ * @brief Collection of common functions
+ * @version 0.1
+ * @date 2025-01
+ *
+ * @copyright Copyright © 2025 dronectl
+ *
+ */
+
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#define min(a, b) a > b ? b : a
+#define max(a, b) a > b ? a : b
+#define array_size(a) (int)(sizeof(a) / sizeof(a[0]))
+
+#endif // __COMMON_H__
