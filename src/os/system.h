d197184048b84135d3e9dcbb2b724eeffd4e8c70
ztnel <christian911@sympatico.ca>
2025-02-02 00:15:11 -0500

    homogenize system task boostrapping and hardware resource distribution

--- src/os/system.h
+++ src/os/system.h
@@ -2,18 +2,27 @@
 #ifndef __SYSTEM_H__
 #define __SYSTEM_H__
 
-/**
- * @brief Hardware spinlock with red led notification
- * @note uses `HAL_Delay` directly to decouple from successful RTOS instantiation
- *
- */
-void system_spinlock(void);
+#define SYSTEM_MAX_TASKS 10
+#define SYSTEM_MAX_TASK_NAME_LEN 10
+
+#include <stdint.h>
 
 /**
- * @brief System operating system health indicator (HID)
- *
+ * @brief Task initialization parameters
  */
-void system_health_indicator(void);
+struct system_task_context {
+  const char name[SYSTEM_MAX_TASK_NAME_LEN];
+  const uint16_t priority;
+  const uint16_t stack_size;
+  const void *init_ctx;
+};
+
+typedef void (*system_task_start)(const struct system_task_context*);
+
+struct system_task {
+  struct system_task_context task_context;
+  const system_task_start start;
+};
 
 /**
  * @brief Operating system startup entry
