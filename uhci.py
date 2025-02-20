7038e2be8de0439918de6e4678ac320cfcf50cf9
ztnel <christian911@sympatico.ca>
2025-02-19 23:07:36 -0500

    udpate uhci client

--- uhci.py
+++ uhci.py
@@ -4,37 +4,31 @@
 Modified: 2025-01
 
 
+
 """
 
-import os
 import logging
 import json
 import socket
 import coloredlogs
 from google.protobuf.json_format import MessageToJson
-from dataclasses import dataclass
-from commands_pb2 import COMMAND_STATUS_OK, CommandRequest, CommandResponse, GetVersionRequest, DiscoveryRequest, DiscoveryResponse
 
+from raptor.v1.commands_pb2 import * 
+from raptor.v1.uhci_pb2 import *
+from raptor.v1.device_pb2 import *
 
-@dataclass
-class RaptorDevice:
-    uuid: int
-    hardware_version: str
-    firmware_version: str
-    ip_address: str
+coloredlogs.install(logging.DEBUG)
+_logger = logging.getLogger(__name__)
 
+class UHCIClient:
 
-class RaptorUHCI:
-
-    UHCI_PORT = 50051  # use a non-wellknown port
-    UHCI_DISCOVERY_PORT = 8000  # use a non-wellknown port
     BUFFER_SIZE = 1024
     DISCOVERY_TIMEOUT_S = 2.0 # two second discovery timeout
 
     def __init__(self, ip_address: str, uuid: int) -> None:
         self._logger = logging.getLogger(__name__)
         self.ip_address = ip_address
-        self.addr = f'{self.ip_address}:{self.UHCI_PORT}'
+        self.addr = f'{self.ip_address}:{UHCI_PORT_COMMAND}'
         self.uuid = uuid
         self.host = socket.gethostname()
         self._logger.info("Instantiated %s", str(self))
@@ -42,49 +36,57 @@ class RaptorUHCI:
     def __repr__(self) -> str:
         return f"UHCI {self.uuid} {self.addr}"
 
-    def send_command(self, command: CommandRequest) -> CommandResponse:
-        serialized_data = command.SerializeToString()
-        self._logger.info("%s -> %s\t%s", self.host, self, json.dumps(MessageToJson(command)))
-        with socket.create_connection((self.ip_address, self.UHCI_PORT)) as sock:
+    def send_command(self, request: UHCIBaseRequest) -> UHCIBaseResponse:
+        serialized_data = request.SerializeToString()
+        self._logger.info("%s -> %s\t%s", self.host, self.addr, MessageToJson(request, indent=None, preserving_proto_field_name=True))
+        with socket.create_connection((self.ip_address, UHCI_PORT_COMMAND)) as sock:
             sock.sendall(serialized_data)
             response_data = sock.recv(self.BUFFER_SIZE)  # Adjust buffer size if necessary
-            response = CommandResponse()
+            response = UHCIBaseResponse()
             response.ParseFromString(response_data)
-        self._logger.info("%s <- %s\t%s", json.dumps(MessageToJson(command)))
+        self._logger.info("%s <- %s\t%s", self.host, self.addr, MessageToJson(response, indent=None, preserving_proto_field_name=True))
         return response
 
     @staticmethod
-    def discover(broadcast_ip: str = '255.255.255.255') -> list[RaptorDevice]:
+    def get_hostname() -> str:
+        return socket.gethostname()
+
+    @staticmethod
+    def discover(broadcast_ip: str = '255.255.255.255') -> list[tuple[str, DeviceMetadata]]:
         devices = []
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) as sock:
             sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-            request = DiscoveryRequest()
+            request = UHCIBaseRequest(
+                uhci=UHCIProtocolRequest(
+                    discovery=UHCIDiscoveryRequest()
+                )
+            )
+            host = UHCIClient.get_hostname()
             serialized_request = request.SerializeToString()
-            sock.sendto(serialized_request, (broadcast_ip, RaptorUHCI.UHCI_DISCOVERY_PORT))
+            _logger.info("%s -> %s\t%s", host,  broadcast_ip, MessageToJson(request, indent=None, preserving_proto_field_name=True))
+            sock.sendto(serialized_request, (broadcast_ip, UHCI_PORT_DISCOVERY))
             # response to discovery must be made within 2-second window
-            sock.settimeout(RaptorUHCI.DISCOVERY_TIMEOUT_S)
+            sock.settimeout(UHCIClient.DISCOVERY_TIMEOUT_S)
             try:
                 while True:
-                    data, addr = sock.recvfrom(RaptorUHCI.BUFFER_SIZE)
-                    response = DiscoveryResponse()
+                    data, addr = sock.recvfrom(UHCIClient.BUFFER_SIZE)
+                    ip_addr, port = addr
+                    response = UHCIBaseResponse()
                     response.ParseFromString(data)
-                    devices.append(
-                        RaptorDevice(
-                            ip_address=addr,
-                            uuid=response.uuid,
-                            hardware_version=response.hardware_version,
-                            firmware_version=response.firmware_version
-                        )
-                    )
+                    _logger.info("%s <- %s\t%s", host, f'{ip_addr}:{port}', MessageToJson(response, indent=None, preserving_proto_field_name=True))
+                    devices.append((ip_addr, response.uhci.discovery.device))
             except socket.timeout:
                 pass
         return devices
 
 
 if __name__ == "__main__":
-    coloredlogs.install(logging.DEBUG)
-    device = RaptorUHCI.discover()[0]
-    raptor = RaptorUHCI(device.ip_address, device.uuid)
-    req = CommandRequest(get_version=GetVersionRequest())
+    ip_addr, meta = UHCIClient.discover()[0]
+    raptor = UHCIClient(ip_addr, meta.uuid)
+    req = UHCIBaseRequest(
+        user=UserRequest(
+            device_metadata=DeviceMetadataRequest()
+        )
+    )
     resp = raptor.send_command(req)
-    assert resp.status == COMMAND_STATUS_OK
+    assert resp.status == UHCI_STATUS_OK
