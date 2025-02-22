611ba6f9ca1574e680d91eee591d29a35b84f016
ztnel <christian911@sympatico.ca>
2025-02-22 14:31:35 -0500

    update sysreg api to conform to new coding standards.

--- src/common/sysreg.c
+++ src/common/sysreg.c
@@ -1,174 +1,22 @@
 /**
  * @file sysreg.c
- * @author ztnel (christian911@sympatio.ca)
  * @brief System Register API
  * @version 0.1
  * @date 2024-03
  *
- * @copyright Copyright © 2024 dronectl
+ * @copyright Copyright © 2025 dronectl
  *
  */
 
 #include "sysreg.h"
-#include <float.h>
+#include "uassert.h"
+#include "common.h"
+
 #include <math.h>
-#include <assert.h>
 #include <string.h>
 
-/**
- * @brief System register data type support
- */
-typedef union {
-  uint8_t u8;
-  uint16_t u16;
-  uint32_t u32;
-  float f32;
-} value_t;
-
-typedef enum {
-  DTYPE_U8,
-  DTYPE_U16,
-  DTYPE_U32,
-  DTYPE_F32,
-} dtype_t;
-
-static sysreg_t registers = {0};
-
-typedef struct reg_conf_t {
-  const size_t offset; // register offset
-  uint8_t access;      // bitfield
-  const dtype_t dtype; // data type
-  const value_t reset; // value on reset
-  const value_t min;   // minimum value
-  const value_t max;   // maximum value
-} reg_conf_t;
-
-// clang-format off
-static reg_conf_t register_config[] = {
-  {
-    .offset = SYSREG_GPU8_UL,
-    .dtype = DTYPE_U8,
-    .access = SYSREG_ACCESS_R | SYSREG_ACCESS_W,
-    .reset = {.u8 = 0},
-    .min = {.u8 = 0},
-    .max = {.u8 = 255}
-  },
-  {
-    .offset = SYSREG_GPU8,
-    .dtype = DTYPE_U8,
-    .access = SYSREG_ACCESS_L | SYSREG_ACCESS_R | SYSREG_ACCESS_W,
-    .reset = {.u8 = 0},
-    .min = {.u8 = 0},
-    .max = {.u8 = 255}
-  },
-  {
-    .offset = SYSREG_GPU16_UL,
-    .dtype = DTYPE_U16,
-    .access = SYSREG_ACCESS_R | SYSREG_ACCESS_W,
-    .reset = {.u16 = 0},
-    .min = {.u16 = 0},
-    .max = {.u16 = 65535}
-  },
-  {
-    .offset = SYSREG_GPU16,
-    .dtype = DTYPE_U16,
-    .access = SYSREG_ACCESS_L | SYSREG_ACCESS_R | SYSREG_ACCESS_W,
-    .reset = {.u16 = 0},
-    .min = {.u16 = 0},
-    .max = {.u16 = 65535}
-  },
-  {
-    .offset = SYSREG_GPU32_UL,
-    .dtype = DTYPE_U32,
-    .access = SYSREG_ACCESS_R | SYSREG_ACCESS_W,
-    .reset = {.u32 = 0},
-    .min = {.u32 = 0},
-    .max = {.u32 = 4294967295}
-  },
-  {
-    .offset = SYSREG_GPU32,
-    .dtype = DTYPE_U32,
-    .access = SYSREG_ACCESS_L | SYSREG_ACCESS_R | SYSREG_ACCESS_W,
-    .reset = {.u32 = 0},
-    .min = {.u32 = 0},
-    .max = {.u32 = 4294967295}
-  },
-  {
-    .offset = SYSREG_GPF32_UL,
-    .dtype = DTYPE_F32,
-    .access = SYSREG_ACCESS_R | SYSREG_ACCESS_W,
-    .reset = {.f32 = 0},
-    .min= {.f32 = -FLT_MAX},
-    .max= {.f32 = FLT_MAX}
-  },
-  {
-    .offset = SYSREG_GPF32,
-    .dtype = DTYPE_F32,
-    .access = SYSREG_ACCESS_L | SYSREG_ACCESS_R | SYSREG_ACCESS_W,
-    .reset = {.f32 = 0},
-    .min= {.f32 = -FLT_MAX},
-    .max= {.f32 = FLT_MAX}
-  },
-  {
-    .offset = SYSREG_HW_VERSION,
-    .dtype = DTYPE_U32,
-    .access = SYSREG_ACCESS_L | SYSREG_ACCESS_R,
-    .reset = {.u32 = SYSREG_HW_VERSION_RESET},
-    .min = {.u32 = 0},
-    .max = {.u32 = 4294967295}
-  },
-  {
-    .offset = SYSREG_FW_VERSION,
-    .dtype = DTYPE_U32,
-    .access = SYSREG_ACCESS_L | SYSREG_ACCESS_R,
-    .reset = {.u32 = SYSREG_FW_VERSION_RESET},
-    .min= {.u32 = 0},
-    .max= {.u32 = 4294967295}
-  },
-  {
-    .offset = SYSREG_UUID,
-    .dtype = DTYPE_U32,
-    .access = SYSREG_ACCESS_L | SYSREG_ACCESS_R,
-    .reset = {.u32 = SYSREG_UUID_RESET},
-    .min = {.u32 = 0},
-    .max = {.u32 = 4294967295}
-  },
-  {
-    .offset = SYSREG_SYS_STAT,
-    .dtype = DTYPE_U8,
-    .access = SYSREG_ACCESS_L | SYSREG_ACCESS_R | SYSREG_ACCESS_W,
-    .reset = {.u8 = SYSREG_SYS_STAT_RESET},
-    .min = {.u8 = 0},
-    .max = {.u8 = 255}
-  },
-  {
-    .offset = SYSREG_STB,
-    .dtype = DTYPE_U8,
-    .access = SYSREG_ACCESS_R | SYSREG_ACCESS_W,
-    .reset = {.u8 = SYSREG_STB_RESET},
-    .min = {.u8 = 0},
-    .max = {.u8 = 255}
-  },
-  {
-    .offset = SYSREG_SETPOINT,
-    .dtype = DTYPE_F32,
-    .access = SYSREG_ACCESS_R | SYSREG_ACCESS_W,
-    .reset = {.f32 = 0.0f},
-    .min = {.f32 = -FLT_MAX},
-    .max = {.f32 = FLT_MAX}
-  },
-};
-// clang-format on
-
-#define SYSREG_NUM_REGISTERS sizeof(register_config) / sizeof(reg_conf_t)
-
-/**
- * @brief Get register configuration
- *
- * @param[in] offset register offset
- * @return pointer to register configuration
- */
-static reg_conf_t *_get_reg_config(const size_t offset);
+// protected register struct
+static struct registers registers = {0};
 
 /**
  * @brief Sanitize register write at offset is permitted by type and access checks
@@ -177,7 +25,7 @@ static reg_conf_t *_get_reg_config(const size_t offset);
  * @param[in] dtype data type of request
  * @return status code
  */
-static sysreg_status_t _sanitize_write(const reg_conf_t *config, const dtype_t dtype);
+static enum sysreg_status sanitize_write(const struct register_config *config, const enum register_dtype dtype);
 
 /**
  * @brief Sanitize register read at offset is permitted by type and access checks
@@ -186,244 +34,196 @@ static sysreg_status_t _sanitize_write(const reg_conf_t *config, const dtype_t d
  * @param[in] dtype data type of request
  * @return status code
  */
-static sysreg_status_t _sanitize_read(const reg_conf_t *config, const dtype_t dtype);
+static enum sysreg_status sanitize_read(const struct register_config *config, const enum register_dtype dtype);
 
-static reg_conf_t *_get_reg_config(const size_t offset) {
-  for (size_t i = 0; i < SYSREG_NUM_REGISTERS; i++) {
-    if (offset == register_config[i].offset) {
-      return &register_config[i];
-    }
-  }
-  return NULL;
-}
-
-static sysreg_status_t _sanitize_write(const reg_conf_t *config, const dtype_t dtype) {
-  if (!(config->access & SYSREG_ACCESS_W)) {
-    return SYSREG_ACCESS_ERR;
+static enum sysreg_status sanitize_write(const struct register_config *config, const enum register_dtype dtype) {
+  if (config->meta.write == 0) {
+    return SYSREG_STATUS_ACCESS_ERR;
   }
   if (config->dtype != dtype) {
-    return SYSREG_DTYPE_ERR;
+    return SYSREG_STATUS_DTYPE_ERR;
   }
-  return SYSREG_OK;
+  return SYSREG_STATUS_OK;
 }
 
-static sysreg_status_t _sanitize_read(const reg_conf_t *config, const dtype_t dtype) {
-  if (!(config->access & SYSREG_ACCESS_R)) {
-    return SYSREG_ACCESS_ERR;
+static enum sysreg_status sanitize_read(const struct register_config *config, const enum register_dtype dtype) {
+  if (config->meta.read == 0) {
+    return SYSREG_STATUS_ACCESS_ERR;
   }
   if (config->dtype != dtype) {
-    return SYSREG_DTYPE_ERR;
+    return SYSREG_STATUS_DTYPE_ERR;
   }
-  return SYSREG_OK;
+  return SYSREG_STATUS_OK;
 }
 
-sysreg_status_t sysreg_init(void) {
-  for (size_t i = 0; i < SYSREG_NUM_REGISTERS; i++) {
-    reg_conf_t config = register_config[i];
-    switch (config.dtype) {
-      case DTYPE_U8:
-        assert(config.reset.u8 >= config.min.u8);
-        assert(config.reset.u8 <= config.max.u8);
+enum sysreg_status sysreg_init(void) {
+  for (size_t i = 0; i < REGISTER_COUNT; i++) {
+    struct register_config *config = &register_config[i];
+    switch (config->dtype) {
+      case REGISTER_DTYPE_U8:
+        uassert(config->reset.u8 >= config->min.u8);
+        uassert(config->reset.u8 <= config->max.u8);
         break;
-      case DTYPE_U16:
-        assert(config.reset.u16 >= config.min.u16);
-        assert(config.reset.u16 <= config.max.u16);
+      case REGISTER_DTYPE_U16:
+        uassert(config->reset.u16 >= config->min.u16);
+        uassert(config->reset.u16 <= config->max.u16);
         break;
-      case DTYPE_U32:
-        assert(config.reset.u32 >= config.min.u32);
-        assert(config.reset.u32 <= config.max.u32);
+      case REGISTER_DTYPE_U32:
+        uassert(config->reset.u32 >= config->min.u32);
+        uassert(config->reset.u32 <= config->max.u32);
         break;
-      case DTYPE_F32:
-        assert(config.reset.f32 >= config.min.f32);
-        assert(config.reset.f32 <= config.max.f32);
+      case REGISTER_DTYPE_F32:
+        uassert(config->reset.f32 >= config->min.f32);
+        uassert(config->reset.f32 <= config->max.f32);
+        break;
+      case REGISTER_DTYPE_U64:
+        uassert(config->reset.u64 >= config->min.u64);
+        uassert(config->reset.u64 <= config->max.u64);
         break;
       default:
-        // CS TODO: log warning - no assertions for missing type
         break;
     }
   }
   sysreg_reset();
-  return SYSREG_OK;
+  return SYSREG_STATUS_OK;
 }
 
-sysreg_status_t sysreg_reset(void) {
-  for (size_t i = 0; i < SYSREG_NUM_REGISTERS; i++) {
+enum sysreg_status sysreg_reset(void) {
+  for (size_t i = 0; i < REGISTER_COUNT; i++) {
     size_t size = 0;
     const void *value = NULL;
-    reg_conf_t config = register_config[i];
-    switch (config.dtype) {
-      case DTYPE_U8:
-        value = &config.reset.u8;
+    struct register_config *config = &register_config[i];
+    switch (config->dtype) {
+      case REGISTER_DTYPE_U8:
+        value = &config->reset.u8;
         size = sizeof(uint8_t);
         break;
-      case DTYPE_U16:
-        value = &config.reset.u16;
+      case REGISTER_DTYPE_U16:
+        value = &config->reset.u16;
         size = sizeof(uint16_t);
         break;
-      case DTYPE_U32:
-        value = &config.reset.u32;
+      case REGISTER_DTYPE_U32:
+        value = &config->reset.u32;
         size = sizeof(uint32_t);
         break;
-      case DTYPE_F32:
-        value = &config.reset.f32;
-        size = sizeof(float_t);
+      case REGISTER_DTYPE_F32:
+        value = &config->reset.f32;
+        size = sizeof(float);
+        break;
+      case REGISTER_DTYPE_U64:
+        value = &config->reset.u64;
+        size = sizeof(uint64_t);
         break;
       default:
         break;
     }
     if (size > 0) {
-      memcpy((uint8_t *)&registers + config.offset, value, size);
+      memcpy((uint8_t *)&registers + config->offset, value, size);
     }
   }
-  return SYSREG_OK;
+  return SYSREG_STATUS_OK;
 }
 
-sysreg_status_t sysreg_set_access(size_t offset, uint8_t access) {
-  reg_conf_t *config = _get_reg_config(offset);
-  if (config == NULL) {
-    return SYSREG_NOT_FOUND_ERR;
-  }
-  // do not allow write of L bit
-  if (access & SYSREG_ACCESS_L) {
-    return SYSREG_OP_ERR;
-  }
-  // check if register access config is locked
-  if (config->access & SYSREG_ACCESS_L) {
-    return SYSREG_ACCESS_ERR;
-  }
-  config->access = access;
-  return SYSREG_OK;
+enum sysreg_status sysreg_lock(const enum register_id id) {
+  struct register_config *config = &register_config[id];
+  config->meta.lock = 1;
+  return SYSREG_STATUS_OK;
 }
 
-sysreg_status_t sysreg_get_u8(size_t offset, uint8_t *data) {
-  sysreg_status_t status;
-  reg_conf_t *config = _get_reg_config(offset);
-  if (config == NULL) {
-    return SYSREG_NOT_FOUND_ERR;
-  }
-  status = _sanitize_read(config, DTYPE_U8);
-  if (status != SYSREG_OK) {
+enum sysreg_status sysreg_unlock(const enum register_id id) {
+  struct register_config *config = &register_config[id];
+  config->meta.lock = 0;
+  return SYSREG_STATUS_OK;
+}
+
+enum sysreg_status sysreg_get_u8(const enum register_id id, uint8_t *data) {
+  struct register_config *config = &register_config[id];
+  enum sysreg_status status = sanitize_read(config, REGISTER_DTYPE_U8);
+  if (status != SYSREG_STATUS_OK) {
     return status;
   }
-  memcpy(data, (uint8_t *)&registers + offset, sizeof(uint8_t));
-  return SYSREG_OK;
+  memcpy(data, (uint8_t *)&registers + config->offset, sizeof(uint8_t));
+  return SYSREG_STATUS_OK;
 }
 
-sysreg_status_t sysreg_set_u8(size_t offset, const uint8_t *data) {
-  sysreg_status_t status;
-  reg_conf_t *config = _get_reg_config(offset);
-  if (config == NULL) {
-    return SYSREG_NOT_FOUND_ERR;
-  }
-  status = _sanitize_write(config, DTYPE_U8);
-  if (status != SYSREG_OK) {
+enum sysreg_status sysreg_set_u8(const enum register_id id, const uint8_t *data) {
+  const struct register_config *config = &register_config[id];
+  enum sysreg_status status = sanitize_write(config, REGISTER_DTYPE_U8);
+  if (status != SYSREG_STATUS_OK) {
     return status;
   }
   uint8_t value = *data;
-  if (value < config->min.u8) {
-    value = config->min.u8;
-  } else if (value > config->max.u8) {
-    value = config->max.u8;
-  }
-  memcpy((uint8_t *)&registers + offset, &value, sizeof(uint8_t));
-  return SYSREG_OK;
+  value = min(value, config->max.u8);
+  value = max(value, config->min.u8);
+  memcpy((uint8_t *)&registers + config->offset, &value, sizeof(uint8_t));
+  return SYSREG_STATUS_OK;
 }
 
-sysreg_status_t sysreg_get_u16(size_t offset, uint16_t *data) {
-  sysreg_status_t status;
-  reg_conf_t *config = _get_reg_config(offset);
-  if (config == NULL) {
-    return SYSREG_NOT_FOUND_ERR;
-  }
-  status = _sanitize_read(config, DTYPE_U16);
-  if (status != SYSREG_OK) {
+enum sysreg_status sysreg_get_u16(const enum register_id id, uint16_t *data) {
+  const struct register_config *config = &register_config[id];
+  enum sysreg_status status = sanitize_read(config, REGISTER_DTYPE_U16);
+  if (status != SYSREG_STATUS_OK) {
     return status;
   }
-  memcpy(data, (uint8_t *)&registers + offset, sizeof(uint16_t));
-  return SYSREG_OK;
+  memcpy(data, (uint8_t *)&registers + config->offset, sizeof(uint16_t));
+  return SYSREG_STATUS_OK;
 }
 
-sysreg_status_t sysreg_set_u16(size_t offset, const uint16_t *data) {
-  sysreg_status_t status;
-  reg_conf_t *config = _get_reg_config(offset);
-  if (config == NULL) {
-    return SYSREG_NOT_FOUND_ERR;
-  }
-  status = _sanitize_write(config, DTYPE_U16);
-  if (status != SYSREG_OK) {
+enum sysreg_status sysreg_set_u16(const enum register_id id, const uint16_t *data) {
+  const struct register_config *config = &register_config[id];
+  enum sysreg_status status = sanitize_write(config, REGISTER_DTYPE_U16);
+  if (status != SYSREG_STATUS_OK) {
     return status;
   }
   uint16_t value = *data;
-  if (value < config->min.u16) {
-    value = config->min.u16;
-  } else if (value > config->max.u16) {
-    value = config->max.u16;
-  }
-  memcpy((uint8_t *)&registers + offset, &value, sizeof(uint16_t));
-  return SYSREG_OK;
+  value = min(value, config->max.u16);
+  value = max(value, config->min.u16);
+  memcpy((uint8_t *)&registers + config->offset, &value, sizeof(uint16_t));
+  return SYSREG_STATUS_OK;
 }
 
-sysreg_status_t sysreg_get_u32(size_t offset, uint32_t *data) {
-  sysreg_status_t status;
-  reg_conf_t *config = _get_reg_config(offset);
-  if (config == NULL) {
-    return SYSREG_NOT_FOUND_ERR;
-  }
-  status = _sanitize_read(config, DTYPE_U32);
-  if (status != SYSREG_OK) {
+enum sysreg_status sysreg_get_u32(const enum register_id id, uint32_t *data) {
+  struct register_config *config = &register_config[id];
+  enum sysreg_status status = sanitize_read(config, REGISTER_DTYPE_U32);
+  if (status != SYSREG_STATUS_OK) {
     return status;
   }
-  memcpy(data, (uint8_t *)&registers + offset, sizeof(uint32_t));
-  return SYSREG_OK;
+  memcpy(data, (uint8_t *)&registers + config->offset, sizeof(uint32_t));
+  return SYSREG_STATUS_OK;
 }
 
-sysreg_status_t sysreg_set_u32(size_t offset, const uint32_t *data) {
-  sysreg_status_t status;
-  reg_conf_t *config = _get_reg_config(offset);
-  if (config == NULL) {
-    return SYSREG_NOT_FOUND_ERR;
-  }
-  status = _sanitize_write(config, DTYPE_U32);
-  if (status != SYSREG_OK) {
+enum sysreg_status sysreg_set_u32(const enum register_id id, const uint32_t *data) {
+  struct register_config *config = &register_config[id];
+  enum sysreg_status status = sanitize_write(config, REGISTER_DTYPE_U32);
+  if (status != SYSREG_STATUS_OK) {
     return status;
   }
   uint32_t value = *data;
-  if (value < config->min.u32) {
-    value = config->min.u32;
-  } else if (value > config->max.u32) {
-    value = config->max.u32;
-  }
-  memcpy((uint8_t *)&registers + offset, &value, sizeof(uint32_t));
-  return SYSREG_OK;
+  value = min(value, config->max.u32);
+  value = max(value, config->min.u32);
+  memcpy((uint8_t *)&registers + config->offset, &value, sizeof(uint32_t));
+  return SYSREG_STATUS_OK;
 }
 
-sysreg_status_t sysreg_get_f32(size_t offset, float *data) {
-  sysreg_status_t status;
-  reg_conf_t *config = _get_reg_config(offset);
-  if (config == NULL) {
-    return SYSREG_NOT_FOUND_ERR;
-  }
-  status = _sanitize_read(config, DTYPE_F32);
-  if (status != SYSREG_OK) {
+enum sysreg_status sysreg_get_f32(const enum register_id id, float *data) {
+  struct register_config *config = &register_config[id];
+  enum sysreg_status status = sanitize_read(config, REGISTER_DTYPE_F32);
+  if (status != SYSREG_STATUS_OK) {
     return status;
   }
-  memcpy(data, (uint8_t *)&registers + offset, sizeof(float));
-  return SYSREG_OK;
+  memcpy(data, (uint8_t *)&registers + config->offset, sizeof(float));
+  return SYSREG_STATUS_OK;
 }
 
-sysreg_status_t sysreg_set_f32(size_t offset, const float *data) {
-  sysreg_status_t status;
-  reg_conf_t *config = _get_reg_config(offset);
-  if (config == NULL) {
-    return SYSREG_NOT_FOUND_ERR;
-  }
-  status = _sanitize_write(config, DTYPE_F32);
-  if (status != SYSREG_OK) {
+enum sysreg_status sysreg_set_f32(const enum register_id id, const float *data) {
+  struct register_config *config = &register_config[id];
+  enum sysreg_status status = sanitize_write(config, REGISTER_DTYPE_F32);
+  if (status != SYSREG_STATUS_OK) {
     return status;
   }
   float value = *data;
   value = fminf(value, config->max.f32);
   value = fmaxf(value, config->min.f32);
-  memcpy((uint8_t *)&registers + offset, &value, sizeof(float));
-  return SYSREG_OK;
+  memcpy((uint8_t *)&registers + config->offset, &value, sizeof(float));
+  return SYSREG_STATUS_OK;
 }
