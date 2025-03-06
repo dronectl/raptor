266e8b4cd6283e85eed353e57bb31ab6c587bb23
ztnel <christian911@sympatico.ca>
2025-03-06 01:38:07 -0500

    start implementing mock system.h calls

--- tests/test_register_config.cc
+++ tests/test_register_config.cc
@@ -1,9 +1,25 @@
 
 #include <gtest/gtest.h>
 
+#include "mock_system.h"
+
 extern "C" {
 #include "registers.h"
 }
+
+class RegisterConfigTestFixture : public ::testing::Test {
+protected:
+  MockSystem m_system;
+
+  void SetUp() override {
+    mock_system = &m_system;
+  }
+
+  void TearDown() override {
+    mock_system = nullptr;
+  }
+};
+
 
 TEST(RegisterConfigTest, RegisterConfigUUID) {
 }
